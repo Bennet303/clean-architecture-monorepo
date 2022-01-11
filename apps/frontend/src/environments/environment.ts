62c70011e173cad7854538f3231c0af8b08b68d5
Bennet Hölscher <bennet.hoelscher@outlook.com>
2022-01-11 16:05:12 +0100

    chore(frontend): add different run configurations

--- apps/frontend/src/environments/environment.ts
+++ apps/frontend/src/environments/environment.ts
@@ -2,9 +2,10 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// Configuration for local environment using local backend
 export const environment = {
   production: false,
-  backendUrl: undefined,
+  backendUrl: 'http://localhost:3333/api/v1',
 };
 
 /*
