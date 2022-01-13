08950693d7eb03f087e0a0c235eea9d05ea690f2
Bennet Hölscher <bennet.hoelscher@outlook.com>
2022-01-13 10:52:50 +0100

    feat(backend): add additional error handling

--- apps/api/src/app/features/manage-users/repositories/manage.users.repository.ts
+++ apps/api/src/app/features/manage-users/repositories/manage.users.repository.ts
@@ -1,8 +1,14 @@
 import { UserDTO } from '../../../core/dtos/user.dto';
 import { FindOneUserParam } from '../../../endpoints/manage-users/params/find.one.user.param';
+import { UserAlreadyExistsError } from '../errors/user.already.exists.error';
+import { UserNotFoundError } from '../errors/user.not.found.error';
 
 export abstract class ManageUsersRepository {
-  abstract createUser(user: UserDTO): Promise<UserDTO | Error>;
-  abstract deleteUser(param: FindOneUserParam): Promise<void | Error>;
-  abstract getUser(): Promise<UserDTO | Error>;
+  abstract createUser(
+    user: UserDTO
+  ): Promise<UserDTO | UserAlreadyExistsError | Error>;
+  abstract deleteUser(
+    param: FindOneUserParam
+  ): Promise<void | UserNotFoundError | Error>;
+  abstract getUser(): Promise<UserDTO | UserNotFoundError | Error>;
 }
