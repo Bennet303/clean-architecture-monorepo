9dcac1cb358263e73a6732754438ec13636d911f
Bennet Hölscher <bennet.hoelscher@outlook.com>
2022-01-13 15:12:19 +0100

    test(backend): add test for manage-user controller

--- apps/api/src/app/endpoints/manage-users/manage.users.controller.ts
+++ apps/api/src/app/endpoints/manage-users/manage.users.controller.ts
@@ -9,9 +9,7 @@ import {
   Logger,
   Param,
   Post,
-  Res,
 } from '@nestjs/common';
-import { Response } from 'express';
 import {
   ApiTags,
   ApiParam,
@@ -23,6 +21,7 @@ import {
   ApiBadRequestResponse,
   ApiNotFoundResponse,
   ApiBody,
+  ApiConflictResponse,
 } from '@nestjs/swagger';
 import { UserDTO } from '../../core/dtos/user.dto';
 import { UserNotFoundError } from '../../features/manage-users/errors/user.not.found.error';
@@ -30,11 +29,12 @@ import { CreateUserUseCase } from '../../features/manage-users/use-cases/create.
 import { DeleteUserUseCase } from '../../features/manage-users/use-cases/delete.user.use.case';
 import { GetUserUseCase } from '../../features/manage-users/use-cases/get.user.use.case';
 import { FindOneUserParam } from './params/find.one.user.param';
+import { UserAlreadyExistsError } from '../../features/manage-users/errors/user.already.exists.error';
 
-@ApiTags('manage-users')
+@ApiTags('users')
 @Controller({
   version: '1',
-  path: '/manage-users',
+  path: '/users',
 })
 export class ManageUsersController {
   logger = new Logger('ManageUserController');
@@ -48,33 +48,39 @@ export class ManageUsersController {
   @Get()
   @ApiOperation({ summary: 'Get the current user.' })
   @ApiOkResponse({ description: 'The current user.', type: UserDTO })
-  @ApiNoContentResponse({
+  @ApiNotFoundResponse({
     description: 'No current user found',
-    type: undefined,
   })
   @ApiInternalServerErrorResponse({ description: 'Internal server error.' })
-  async getUser(
-    @Res({ passthrough: true }) response: Response
-  ): Promise<UserDTO | undefined> {
+  async getUser(): Promise<UserDTO | undefined> {
     const res = await this.getUserUC.execute();
 
-    if (!res) response.status(HttpStatus.NO_CONTENT);
-
-    if (res instanceof Error)
+    if (res instanceof UserNotFoundError)
+      throw new HttpException('No current user found', HttpStatus.NOT_FOUND);
+    else if (res instanceof Error)
       throw new HttpException(res.message, HttpStatus.INTERNAL_SERVER_ERROR);
 
     return res;
   }
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   @ApiOperation({ summary: 'Create a new user.' })
   @ApiBody({ type: UserDTO })
   @ApiCreatedResponse({ description: 'The created user.', type: UserDTO })
+  @ApiConflictResponse({ description: 'User already exists.' })
   @ApiBadRequestResponse({ description: 'Invalid user.' })
   @ApiInternalServerErrorResponse({ description: 'Internal server error.' })
   async createUser(@Body() user: UserDTO): Promise<UserDTO> {
     this.logger.log(`Creating user: ${JSON.stringify(user)}`);
-    return user;
+    const res = await this.createUserUC.execute(user);
+
+    if (res instanceof UserAlreadyExistsError)
+      throw new HttpException(res.message, HttpStatus.CONFLICT);
+    else if (res instanceof Error)
+      throw new HttpException(res.message, HttpStatus.INTERNAL_SERVER_ERROR);
+
+    return res;
   }
 
   @Delete('/:id')
@@ -90,7 +96,7 @@ export class ManageUsersController {
   @ApiBadRequestResponse({ description: 'Invalid params.' })
   @ApiInternalServerErrorResponse({ description: 'Internal server error.' })
   async deleteUser(@Param() params: FindOneUserParam): Promise<void> {
-    const res = this.deleteUserUC.execute(params);
+    const res = await this.deleteUserUC.execute(params);
 
     if (res instanceof UserNotFoundError)
       throw new HttpException(res.message, HttpStatus.NOT_FOUND);
