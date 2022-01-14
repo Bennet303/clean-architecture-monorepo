7c5dbc8d177715d33b96c6bac71adb35582a6c04
Bennet Hölscher <bennet.hoelscher@outlook.com>
2022-01-14 09:33:14 +0100

    fix(backend): update dtos to be inline with entities

--- apps/api/src/app/core/dtos/user.dto.ts
+++ apps/api/src/app/core/dtos/user.dto.ts
@@ -1,4 +1,17 @@
 import { UserEntity } from '@clean-architecture-monorepo/shared';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
 import { DTO } from './dto';
 
-export class UserDTO extends DTO implements UserEntity {}
+export class UserDTO extends DTO implements UserEntity {
+  @ApiProperty({ example: '1' })
+  @IsNotEmpty()
+  @IsString()
+  @Length(1, 10)
+  id: string;
+
+  constructor(obj: UserDTO) {
+    super();
+    Object.assign(this, obj);
+  }
+}
