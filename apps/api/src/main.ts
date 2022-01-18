62f522b19a164da31fc7be921833d130481445e7
Bennet Hölscher <bennet.hoelscher@outlook.com>
2022-01-18 09:29:01 +0100

    feat(backend): enable transformation in the validation pipe

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -35,6 +35,7 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       disableErrorMessages: !environment.local, //! change to 'true' for production
+      transform: true,
     })
   );
 
